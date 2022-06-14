a1bdbf009fc798f7bec3f09e3aa5fa2de1f90702
egsch <egschaumann@gmail.com>
2022-06-13 19:09:08 -0500

    Replace call graph generation method to allow option configuration

--- src/main/java/JSCallGraphDriver.java
+++ src/main/java/JSCallGraphDriver.java
@@ -9,16 +9,26 @@ import java.nio.file.Paths;
 import java.io.FileWriter;
 import java.util.Iterator;
 
+import com.ibm.wala.cast.js.ipa.callgraph.JSAnalysisOptions;
+import com.ibm.wala.cast.js.ipa.callgraph.JSCFABuilder;
 import com.ibm.wala.cast.js.ipa.callgraph.JSCallGraphUtil;
+import com.ibm.wala.cast.js.loader.JavaScriptLoader;
+import com.ibm.wala.cast.js.loader.JavaScriptLoaderFactory;
 import com.ibm.wala.cast.js.translator.CAstRhinoTranslatorFactory;
 import com.ibm.wala.cast.js.util.JSCallGraphBuilderUtil;
+import com.ibm.wala.cast.loader.SingleClassLoaderFactory;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
 import com.ibm.wala.ipa.callgraph.CGNode;
 import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.ipa.callgraph.CallGraphBuilder;
 import com.ibm.wala.ipa.callgraph.CallGraphStats;
+import com.ibm.wala.ipa.callgraph.Entrypoint;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
 import com.ibm.wala.ssa.SSAInstruction;
 import com.ibm.wala.util.CancelException;
 import com.ibm.wala.util.WalaException;
 import com.ibm.wala.classLoader.CallSiteReference;
+import com.ibm.wala.classLoader.Language;
 import com.ibm.wala.classLoader.ProgramCounter;
 
 public class JSCallGraphDriver {
@@ -30,12 +40,23 @@ public class JSCallGraphDriver {
 	 * @throws CancelException 
 	 * @throws IOException 
 	 * @throws IllegalArgumentException 
+	 * 
 	 */
 	public static void main(String[] args) throws IllegalArgumentException, IOException, CancelException, WalaException {
+		// Sets up JS Call Graph Builder
 		Path inPath = Paths.get(args[0]);
 		JSCallGraphUtil.setTranslatorFactory(new CAstRhinoTranslatorFactory());
-		CallGraph CG = JSCallGraphBuilderUtil.makeScriptCG(
-				inPath.getParent().toString(), inPath.getFileName().toString());
+		JavaScriptLoaderFactory loaders = JSCallGraphBuilderUtil.makeLoaders();
+		AnalysisScope scope = JSCallGraphBuilderUtil.makeScope(JSCallGraphBuilderUtil.makeSourceModules(inPath.getParent().toString(), inPath.getFileName().toString(), JSCallGraphBuilderUtil.class.getClassLoader()), loaders, JavaScriptLoader.JS);
+		IClassHierarchy cha = JSCallGraphBuilderUtil.makeHierarchy(scope, loaders);
+		Iterable<Entrypoint> e = JSCallGraphBuilderUtil.makeScriptRoots(cha);
+		//Configures Options
+		JSAnalysisOptions options = new JSAnalysisOptions(scope, e);
+		//In progress: option integration, ex. options.setReflectionOptions(null);
+		
+		JSCFABuilder builder = JSCallGraphBuilderUtil.makeScriptCGBuilder(inPath.getParent().toString(), inPath.getFileName().toString());
+		//Builds Call Graph
+		CallGraph CG = builder.makeCallGraph(options);
 		System.out.println(CallGraphStats.getStats(CG));
 		FileWriter fw = new FileWriter(args[1]);
         for (CGNode cgn : CG) {
